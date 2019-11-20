35405ac325153e9c6c1ea63d9a6d1b81b0f7de02
Kacper <kacper.szatan@gmail.com>
2019-11-20 12:14:55 +0100

    main structure of project added

---
+++ Nonogram/src/nonogram/game/edu/Menu/MainMenu.java
@@ -0,0 +1,23 @@
+package nonogram.game.edu.Menu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainMenu extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainMenuLayout.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 600, 525));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
